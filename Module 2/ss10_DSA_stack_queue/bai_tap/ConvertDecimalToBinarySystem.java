569233aa2607569f8e60c7146d73f4f46210b308
Khanhx1 <nguyentukhanh2001@gmail.com>
2023-11-16 20:48:04 +0700

    huhu

---
+++ Module 2/ss10_DSA_stack_queue/bai_tap/ConvertDecimalToBinarySystem.java
@@ -0,0 +1,27 @@
+package ss10_DSA_stack_queue.bai_tap;
+
+import java.util.Stack;
+import java.util.Scanner;
+
+public class ConvertDecimalToBinarySystem {
+    public static void main(String[] args) {
+        String binary = "";
+        System.out.println("Enter decimal number:");
+        Scanner scanner = new Scanner(System.in);
+        int divident = scanner.nextInt();
+        int quotient;
+        int remainder;
+        final int DIVISOR = 2;
+        Stack<Integer> stack = new Stack<>();
+        while (divident != 0) {
+            quotient = divident / DIVISOR;
+            remainder = divident % DIVISOR;
+            stack.push(remainder);
+            divident = quotient;
+        }
+        while (!stack.isEmpty()) {
+            binary += stack.pop();
+        }
+        System.out.println(binary);
+    }
+}
